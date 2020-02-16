4b520aff12ca4b5617f6d6c2abd62f6e503fafa4
gonzalo10 <gonzalo.achiaga@gmail.com>
2020-02-16 18:05:06 +0100

    Updates

---
+++ precache-manifest.c584c0896feb8d2e070346c47e6cc4d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f029d4588c4a671a9a5cf24bffde0c",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "ae963abd393be06b4378",
+    "url": "/pwa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7a2ea6950455b4fec1a3",
+    "url": "/pwa/static/js/2.b719b4ec.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pwa/static/js/2.b719b4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae963abd393be06b4378",
+    "url": "/pwa/static/js/main.83c4d638.chunk.js"
+  },
+  {
+    "revision": "2fc8c0e2321f77c5e22b",
+    "url": "/pwa/static/js/runtime-main.9a9f77ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
